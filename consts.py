1c6260ba65a31944b95cd841a779445a2e2e94c6
BerryLiu2002 <70964424+BerryLiu2002@users.noreply.github.com>
2022-09-20 09:57:19 -0400

    Removed personal details from consts file

--- consts.py
+++ consts.py
@@ -1,5 +1,5 @@
 # Complete path to directory containing NYU Engage csvs
-csv_dir = r"C:\Users\BerryLiu\Desktop\Projects\Club-Attendance-Tracker\spreadsheets"
+csv_dir = r""
 
 # Complete path to attendance.csv file
-attendance_path = r"C:\Users\BerryLiu\Desktop\Projects\Club-Attendance-Tracker\attendance.csv"
+attendance_path = r""
