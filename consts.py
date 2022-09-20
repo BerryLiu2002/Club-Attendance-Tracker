e566fe8dc9fee8cd6ab2f7e98864f9f473a9f134
Berry Liu <bl3056@nyu.edu>
2022-09-19 23:03:22 -0400

    Added descriptive comments to identify constants

--- consts.py
+++ consts.py
@@ -1,2 +1,5 @@
+# Complete path to directory containing NYU Engage csvs
 csv_dir = r"C:\Users\BerryLiu\Desktop\Projects\Club-Attendance-Tracker\spreadsheets"
+
+# Complete path to attendance.csv file
 attendance_path = r"C:\Users\BerryLiu\Desktop\Projects\Club-Attendance-Tracker\attendance.csv"
